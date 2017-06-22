c6c07b79a2c799379f765e5bae8b309f1a6d289d
yyhuo <szyuanyue@126.com>
2017-06-23 00:09:09 +0800

    顶层菜单

--- reader.py
+++ reader.py
@@ -1,5 +1,10 @@
-#这是程序的主题
-# -*- coding:utf-8 -*-  
+# 这是程序的主题
+# -*- coding:utf-8 -*-
 
 from tkinter import *
 from tkinter.ttk import *
+
+root = Tk()
+root.wm_title('简单小说阅读器')
+menubar = Menu(root)
+root.mainloop()
